ad5f3fadfba56f0978fe9f7019dd50db88b7151b
ZiaCheema <ziaurrehman1145@gmail.com>
2026-01-13 02:09:38 +0500

    remove loop from verilog

--- hardware/single_cycle_rv32i_core/register_file.v
+++ hardware/single_cycle_rv32i_core/register_file.v
@@ -17,9 +17,38 @@ module reg_file (
 
     always @(rst) begin
         if (rst) begin
-            for (integer i = 0; i < 32; i = i + 1) begin
-                registers[i] <= 32'd0;
-            end
+            registers[0] <= 32'd0;
+            registers[1] <= 32'd0;
+            registers[2] <= 32'd0;
+            registers[3] <= 32'd0;
+            registers[4] <= 32'd0;
+            registers[5] <= 32'd0;
+            registers[6] <= 32'd0;
+            registers[7] <= 32'd0;
+            registers[8] <= 32'd0;
+            registers[9] <= 32'd0;
+            registers[10] <= 32'd0;
+            registers[11] <= 32'd0;
+            registers[12] <= 32'd0;
+            registers[13] <= 32'd0;
+            registers[14] <= 32'd0;
+            registers[15] <= 32'd0;
+            registers[16] <= 32'd0;
+            registers[17] <= 32'd0;
+            registers[18] <= 32'd0;
+            registers[19] <= 32'd0;
+            registers[20] <= 32'd0;
+            registers[21] <= 32'd0;
+            registers[22] <= 32'd0;
+            registers[23] <= 32'd0;
+            registers[24] <= 32'd0;
+            registers[25] <= 32'd0;
+            registers[26] <= 32'd0;
+            registers[27] <= 32'd0;
+            registers[28] <= 32'd0;
+            registers[29] <= 32'd0;
+            registers[30] <= 32'd0;
+            registers[31] <= 32'd0;
         end 
     end
 
