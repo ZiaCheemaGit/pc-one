16297073347bbe291420e098df1cae3aac8fde3a
ZiaCheema <ziaurrehman1145@gmail.com>
2026-01-12 21:06:32 +0500

    restructured repo

--- noxfile.py
+++ noxfile.py
@@ -10,10 +10,9 @@ def install_deps(session: nox.Session) -> None:
     session.log("Installing all python dependencies for tests")
     session.install(*all_python_deps)
 
-
+@nox.session
 def test_single_cycle_rv32i_core(session: nox.Session) -> None:
 
-    session.log("Entering Directory single_cycle_rv32i_core")
     session.chdir("tests/single_cycle_rv32i_core/")
 
     # build hex file
@@ -23,11 +22,10 @@ def test_single_cycle_rv32i_core(session: nox.Session) -> None:
 
     session.run("make", external=True)
 
-    session.log("Leaving Directory single_cycle_rv32i_core")
-    session.chdir("../../")    
+    session.chdir("../../")        
 
 
-@nox.session
+
 def test_build(session: nox.Session) -> None:
 
     # Install all dependencies to run tests
