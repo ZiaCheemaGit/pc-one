9d79c92bde9740298efcddf3e692b7d778d27a6f
Zia <root@DESKTOP-DKJK6L5.localdomain>
2026-02-05 22:49:26 +0500

    create an ISE project for FPGA uart testing

--- hardware/single_cycle_rv32i_core/register_file.v
+++ hardware/single_cycle_rv32i_core/register_file.v
@@ -15,42 +15,54 @@ module reg_file (
 
     reg [31:0] registers [0:31];
 
-    always @(rst) begin
+    integer i;
+
+    // WRITE + RESET (single clocked block)
+    always @(posedge clk or posedge rst) begin
         if (rst) begin
-            registers[0] <= 32'd0;
-            registers[1] <= 32'd0;
-            registers[2] <= 32'd0;
-            registers[3] <= 32'd0;
-            registers[4] <= 32'd0;
-            registers[5] <= 32'd0;
-            registers[6] <= 32'd0;
-            registers[7] <= 32'd0;
-            registers[8] <= 32'd0;
-            registers[9] <= 32'd0;
-            registers[10] <= 32'd0;
-            registers[11] <= 32'd0;
-            registers[12] <= 32'd0;
-            registers[13] <= 32'd0;
-            registers[14] <= 32'd0;
-            registers[15] <= 32'd0;
-            registers[16] <= 32'd0;
-            registers[17] <= 32'd0;
-            registers[18] <= 32'd0;
-            registers[19] <= 32'd0;
-            registers[20] <= 32'd0;
-            registers[21] <= 32'd0;
-            registers[22] <= 32'd0;
-            registers[23] <= 32'd0;
-            registers[24] <= 32'd0;
-            registers[25] <= 32'd0;
-            registers[26] <= 32'd0;
-            registers[27] <= 32'd0;
-            registers[28] <= 32'd0;
-            registers[29] <= 32'd0;
-            registers[30] <= 32'd0;
-            registers[31] <= 32'd0;
-        end 
+            for (i = 0; i < 32; i = i + 1)
+                registers[i] <= 32'd0;
+        end else begin
+            if (reg_write_control && dest_reg != 5'd0)
+                registers[dest_reg] <= reg_write_data;
+        end
     end
+    // always @(rst) begin
+    //     if (rst) begin
+    //         registers[0] <= 32'd0;
+    //         registers[1] <= 32'd0;
+    //         registers[2] <= 32'd0;
+    //         registers[3] <= 32'd0;
+    //         registers[4] <= 32'd0;
+    //         registers[5] <= 32'd0;
+    //         registers[6] <= 32'd0;
+    //         registers[7] <= 32'd0;
+    //         registers[8] <= 32'd0;
+    //         registers[9] <= 32'd0;
+    //         registers[10] <= 32'd0;
+    //         registers[11] <= 32'd0;
+    //         registers[12] <= 32'd0;
+    //         registers[13] <= 32'd0;
+    //         registers[14] <= 32'd0;
+    //         registers[15] <= 32'd0;
+    //         registers[16] <= 32'd0;
+    //         registers[17] <= 32'd0;
+    //         registers[18] <= 32'd0;
+    //         registers[19] <= 32'd0;
+    //         registers[20] <= 32'd0;
+    //         registers[21] <= 32'd0;
+    //         registers[22] <= 32'd0;
+    //         registers[23] <= 32'd0;
+    //         registers[24] <= 32'd0;
+    //         registers[25] <= 32'd0;
+    //         registers[26] <= 32'd0;
+    //         registers[27] <= 32'd0;
+    //         registers[28] <= 32'd0;
+    //         registers[29] <= 32'd0;
+    //         registers[30] <= 32'd0;
+    //         registers[31] <= 32'd0;
+    //     end 
+    // end
 
     always @(posedge clk) begin
         if (reg_write_control && dest_reg != 5'd0 && !rst) begin
