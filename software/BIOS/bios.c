c2ec2fdf59b729457d24070c48d179c9cbd50984
ZiaCheemaGit <ziaurrehman1145@gmail.com>
2026-04-04 03:45:53 +0500

    Fix CI

--- software/BIOS/bios.c
+++ software/BIOS/bios.c
@@ -3,14 +3,39 @@
 # include "../include/vga.h"
 # include "../include/time.h"
 
-void loadBootLoader(){
+// void int_to_string(int num, char *str) {
+//     int i = 0, temp = num;
 
-}
+//     if (num == 0) {
+//         str[i] = '0';
+//         i++;
+//         str[i] = '\0';
+//         return;
+//     }
+
+//     while (temp > 0) {
+//         str[i++] = (temp % 10) + '0';
+//         temp /= 10;
+//     }
+
+//     str[i] = '\0';
+
+//     // reverse string
+//     for (int j = 0; j < i/2; j++) {
+//         char t = str[j];
+//         str[j] = str[i-j-1];
+//         str[i-j-1] = t;
+//     }
+// }
+
+// void loadBootLoader(){
+
+// }
 
 int bios(){
     uart_println("Hello from PC-ONE!");
     delay_second(1);
-    for (int i = 0; i < 2; i++){
+    for (int i = 0; i < 4; i++){
         uart_println("Wait 3 second"); 
         delay_second(3);
         
@@ -20,15 +45,6 @@ int bios(){
         uart_putc(c);
         uart_println("");
     }
-
-    int max = 100;
-    int min = 36;
-    int threshold = max - min;
-    uart_print("Threshold: ");
-    char s[10];
-    int_to_string(threshold, s);
-    uart_println(s);
-
 
     while(1){}
 
