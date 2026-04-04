492bc96c7e7a29dec9616aada2fc8794e2d40c93
ZiaCheemaGit <ziaurrehman1145@gmail.com>
2026-04-05 00:06:49 +0500

    Fix bios.c formatting

--- software/BIOS/bios.c
+++ software/BIOS/bios.c
@@ -3,35 +3,6 @@
 # include "../include/vga.h"
 # include "../include/time.h"
 
-// void int_to_string(int num, char *str) {
-//     int i = 0, temp = num;
-
-//     if (num == 0) {
-//         str[i] = '0';
-//         i++;
-//         str[i] = '\0';
-//         return;
-//     }
-
-//     while (temp > 0) {
-//         str[i++] = (temp % 10) + '0';
-//         temp /= 10;
-//     }
-
-//     str[i] = '\0';
-
-//     // reverse string
-//     for (int j = 0; j < i/2; j++) {
-//         char t = str[j];
-//         str[j] = str[i-j-1];
-//         str[i-j-1] = t;
-//     }
-// }
-
-// void loadBootLoader(){
-
-// }
-
 int bios(){
     uart_println("Hello from PC-ONE!");
     delay_second(1);
