ed752fdcfe5b8bc6fcfdcb8ceb4c54b6b970871e
ZiaCheemaGit <ziaurrehman1145@gmail.com>
2026-06-19 19:34:09 +0500

    compile the failing test into assembly(issue #14)

--- tests/hardware/pc_one/test_cases/c-cpp_tests/test_aggressive_c.c
+++ tests/hardware/pc_one/test_cases/c-cpp_tests/test_aggressive_c.c
@@ -39,6 +39,28 @@ void int_to_string(int num, char *str) {
     }
 }
 
+// Function to debug
+// void int_to_string(int num, char *str) {
+//     int i = 0, temp = num;
+//     if (num == 0) {
+//         str[i] = '0';
+//         i++;
+//         str[i] = '\0';
+//         return;
+//     }
+//     while (temp > 0) {
+//         str[i++] = (temp % 10) + '0';
+//         temp /= 10;
+//     }
+//     str[i] = '\0';
+//     // reverse string
+//     for (int j = 0; j < i/2; j++) {
+//         char t = str[j];
+//         str[j] = str[i-j-1];
+//         str[i-j-1] = t;
+//     }
+// }
+
 int bios(){
     char s[10];
     int_to_string(129, s);
