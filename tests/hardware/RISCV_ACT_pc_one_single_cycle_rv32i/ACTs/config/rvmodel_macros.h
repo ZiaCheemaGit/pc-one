75b322ed49ccb2cadf67c02667e2990948ddc720
ZiaCheemaGit <ziaurrehman1145@gmail.com>
2026-09-07 18:52:10 +0500

    WIP

---
+++ tests/hardware/RISCV_ACT_pc_one_single_cycle_rv32i/ACTs/config/rvmodel_macros.h
@@ -0,0 +1,45 @@
+#ifndef _RVMODEL_MACROS_H
+#define _RVMODEL_MACROS_H
+
+#define RVMODEL_DATA_SECTION \
+        .pushsection .tohost,"aw",@progbits;                \
+        .balign 8; .global tohost; tohost: .dword 0;         \
+        .balign 8; .global fromhost; fromhost: .dword 0;     \
+        .popsection;
+
+#define RVMODEL_BOOT 
+#define RVMODEL_IO_INIT(_R1, _R2, _R3)
+#define RVMODEL_IO_WRITE_STR(_R1, _R2, _R3, _STR_PTR)
+
+#define RVMODEL_HALT_PASS  \
+  li x1, 1                ;\
+  la t0, tohost           ;\
+  sw x1, 0(t0)            ;\
+  sw x0, 4(t0)            ;\
+  1: j 1b                 ;\
+
+#define RVMODEL_HALT_FAIL \
+  li x1, 3                ;\
+  la t0, tohost           ;\
+  sw x1, 0(t0)            ;\
+  sw x0, 4(t0)            ;\
+  1: j 1b                 ;\
+
+/* Dummy defines to appease check_defines.h */
+#define RVMODEL_INTERRUPT_LATENCY 10
+#define RVMODEL_TIMER_INT_SOON_DELAY 1000
+#define RVMODEL_ACCESS_FAULT_ADDRESS 0x00000000
+#define RVMODEL_MTIME_ADDRESS 0x00000000
+#define RVMODEL_MTIMECMP_ADDRESS 0x00000000
+#define RVMODEL_MSIP_ADDRESS 0x00000000
+
+#define RVMODEL_SET_MEXT_INT(_R1, _R2)
+#define RVMODEL_CLR_MEXT_INT(_R1, _R2)
+#define RVMODEL_SET_MSW_INT(_R1, _R2)
+#define RVMODEL_CLR_MSW_INT(_R1, _R2)
+#define RVMODEL_SET_SEXT_INT(_R1, _R2)
+#define RVMODEL_CLR_SEXT_INT(_R1, _R2)
+#define RVMODEL_SET_SSW_INT(_R1, _R2)
+#define RVMODEL_CLR_SSW_INT(_R1, _R2)
+
+#endif // _RVMODEL_MACROS_H
---
+++ tests/hardware/RISCV_ACT_pc_one_single_cycle_rv32i/ACTs/config/rvtest_config.svh
@@ -0,0 +1,6 @@
+`ifndef _RVTEST_CONFIG_SVH
+`define _RVTEST_CONFIG_SVH
+
+`define UDB_MXLEN 32
+
+`endif // _RVTEST_CONFIG_SVH
