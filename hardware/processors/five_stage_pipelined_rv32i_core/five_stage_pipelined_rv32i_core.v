0d78a821c8433d365193cc4fc8f78086e753a3f0
ZiaCheemaGit <ziaurrehman1145@gmail.com>
2026-09-10 23:09:12 +0500

    route actual functions

--- hardware/processors/five_stage_pipelined_rv32i_core/five_stage_pipelined_rv32i_core.v
+++ hardware/processors/five_stage_pipelined_rv32i_core/five_stage_pipelined_rv32i_core.v
@@ -55,39 +55,55 @@ module five_stage_pipelined_rv32i_core(
         .s_type_immediate(s_type_immediate)
     );
     
-    wire func3, reg_write_control, mem_write_from_control_unit, byte_op_from_control_unit;
+    wire func3, reg_write_control_from_control_unit, unsigned_op_from_control_unit, byte_op_from_control_unit, half_op_from_control_unit;
     wire [1:0] alu_op_control, pc_src, alu_src_control;
-    wire [2:0] mem_to_reg_control;
+    wire [2:0] mem_to_reg_control_from_control_unit;
+    assign byte_op = byte_op_from_control_unit;
+    assign half_op = half_op_from_control_unit;
     control_unit control_unit_instance(
         .opcode(instruction_from_if_id[6:0]),
         .func3(instruction_from_if_id[14:12]), 
-        .mem_read(mem_read_from_control_unit), 
-        .mem_write(mem_write_from_control_unit), 
+        .mem_read(mem_read), 
+        .mem_write(mem_write), 
         .alu_src(alu_src_control), 
-        .reg_write(reg_write_control),
+        .reg_write(reg_write_control_from_control_unit),
         .alu_op(alu_op_control), 
-        .mem_to_reg(mem_to_reg_control),
+        .mem_to_reg(mem_to_reg_control_from_control_unit),
         .pc_src(pc_src),
         .byte_op(byte_op_from_control_unit),
         .half_op(half_op_from_control_unit),
         .unsigned_op(unsigned_op_from_control_unit)
     );
 
-    wire byte_op_from_ex_mem, half_op_from_ex_mem, unsigned_op_from_control_unit;
-    assign byte_op = byte_op_from_ex_mem;
-    assign half_op = half_op_from_ex_mem;
-    assign unsigned_op = unsigned_op_from_control_unit;
+    wire [31:0] mem_address_from_ex_mem, alu_result_to_ex_mem, alu_result_from_ex_mem,
+    pc_plus_4, pc_plus_4_from_ex_mem, pc_plus_u_type_immediate, pc_plus_u_type_immediate_from_ex_mem,
+    u_type_immediate_from_ex_mem;
+    wire [4:0] dest_reg_from_ex_mem;
+    wire [2:0] mem_to_reg_control_from_ex_mem;
+    wire byte_op_from_ex_mem, half_op_from_ex_mem;
     ex_mem_reg ex_mem_reg_instance(
+        .alu_result_in(alu_result_to_ex_mem),
+        .alu_result_out(alu_result_from_ex_mem),
         .byte_op_in(byte_op_from_control_unit),
         .byte_op_out(byte_op_from_ex_mem),
         .half_op_in(half_op_from_control_unit),
         .half_op_out(half_op_from_ex_mem),
         .unsigned_op_in(unsigned_op_from_control_unit),
         .unsigned_op_out(unsigned_op_from_ex_mem),
-        .mem_write_in(mem_write_from_control_unit),
-        .mem_read_in(mem_read_from_control_unit),
-        .mem_write_out(mem_write),
-        .mem_read_out(mem_read)
+        .mem_address_in(mem_address),
+        .mem_address_out(mem_address_from_ex_mem),
+        .reg_write_control_in(reg_write_control_from_control_unit),
+        .reg_write_control_out(reg_write_control_from_ex_mem),
+        .dest_reg_in(instruction_from_if_id[11:7]),
+        .dest_reg_out(dest_reg_from_ex_mem),
+        .mem_to_reg_control_in(mem_to_reg_control_from_control_unit),
+        .mem_to_reg_control_out(mem_to_reg_control_from_ex_mem),
+        .pc_plus_4_in(pc_plus_4),
+        .pc_plus_4_out(pc_plus_4_from_ex_mem),
+        .pc_plus_u_type_immediate_in(pc_plus_u_type_immediate),
+        .pc_plus_u_type_immediate_out(pc_plus_u_type_immediate_from_ex_mem),
+        .u_type_immediate_in(u_type_immediate),
+        .u_type_immediate_out(u_type_immediate_from_ex_mem)
     );
 
     wire [31:0] load_op_data;
@@ -95,7 +111,7 @@ module five_stage_pipelined_rv32i_core(
         .byte_op(byte_op_from_ex_mem),
         .half_op(half_op_from_ex_mem),
         .unsigned_op(unsigned_op_from_ex_mem),
-        .byte_offset(mem_address[1:0]),
+        .byte_offset(mem_address_from_ex_mem[1:0]),
         .data_from_mem(mem_data_from_mem),
         .op_data(load_op_data)
     );
@@ -121,11 +137,11 @@ module five_stage_pipelined_rv32i_core(
     reg_file reg_file_instance(
         .clk(clk),
         .rst(rst),
-        .dest_reg(instruction_from_if_id[11:7]), 
+        .dest_reg(dest_reg_from_ex_mem), 
         .src1_reg(instruction_from_if_id[19:15]), 
         .src2_reg(instruction_from_if_id[24:20]),
         .reg_write_data(reg_write_data),
-        .reg_write_control(reg_write_control),
+        .reg_write_control(reg_write_control_from_ex_mem),
         .src1_reg_value(rs1), 
         .src2_reg_value(rs2)
     );
@@ -151,8 +167,9 @@ module five_stage_pipelined_rv32i_core(
         .out(alu_out)
     );
     assign mem_address = alu_out;
+    assign alu_result_to_ex_mem = alu_out;
     
-    wire [31:0] pc_plus_4;
+    
     adder32 adder32_instance_const4(
         .in1(4),
         .in2(pc_out_add_from_if_id),
@@ -185,7 +202,6 @@ module five_stage_pipelined_rv32i_core(
         .out(pc_jump_add)
     );
     
-    wire [31:0] pc_plus_u_type_immediate;
     adder32 u_type_adder(
         .in1(u_type_immediate),
         .in2(pc_out_add_from_if_id),
@@ -193,12 +209,12 @@ module five_stage_pipelined_rv32i_core(
     );
        
     mux_5x1 reg_write_mux(
-        .in0(alu_out),
+        .in0(alu_result_from_ex_mem),
         .in1(load_op_data),
-        .in2(pc_plus_4), 
-        .in3(u_type_immediate), 
-        .in4(pc_plus_u_type_immediate),
-        .sel(mem_to_reg_control),
+        .in2(pc_plus_4_from_ex_mem), 
+        .in3(u_type_immediate_from_ex_mem), 
+        .in4(pc_plus_u_type_immediate_from_ex_mem),
+        .sel(mem_to_reg_control_from_ex_mem),
         .out(reg_write_data)
     );
            
